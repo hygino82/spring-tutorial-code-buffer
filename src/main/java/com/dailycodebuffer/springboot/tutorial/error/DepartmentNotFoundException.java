dc10089bfd0eaad8e3e4d1b519146a0d1fff708b
Adroaldo Ferreira <hygino@msn.com>
2022-02-05 20:44:56 -0300

    Changing H2 db to MySQL

--- src/main/java/com/dailycodebuffer/springboot/tutorial/error/DepartmentNotFoundException.java
+++ src/main/java/com/dailycodebuffer/springboot/tutorial/error/DepartmentNotFoundException.java
@@ -1,8 +1,9 @@
 package com.dailycodebuffer.springboot.tutorial.error;
 
 public class DepartmentNotFoundException extends Exception{
+	private static final long serialVersionUID = 1L;
 
-    public DepartmentNotFoundException() {
+	public DepartmentNotFoundException() {
         super();
     }
 
