109147233a5ea55980c90d04dfb330d7ccb64cec
Adroaldo Ferreira <hygino@msn.com>
2022-02-05 15:33:25 -0300

    Update department by Id

--- src/main/java/com/dailycodebuffer/springboot/tutorial/service/DepartmentServiceImpl.java
+++ src/main/java/com/dailycodebuffer/springboot/tutorial/service/DepartmentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dailycodebuffer.springboot.tutorial.service;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,29 @@ public class DepartmentServiceImpl implements DepartmentService {
 	@Override
 	public void deleteDepartmentById(Long departmentId) {
 		departmentRepository.deleteById(departmentId);
+	}
+
+	@Override
+	public Department updateDepartment(Long departmentId, Department department) {
+		Department depDB = departmentRepository.findById(departmentId).get();
+		
+		if (Objects.nonNull(department.getDepartmentName()) &&
+				!"".equalsIgnoreCase(department.getDepartmentName())) {
+			depDB.setDepartmentName(department.getDepartmentName());	
+		}
+		
+		if (Objects.nonNull(department.getDepartmentCode()) &&
+				!"".equalsIgnoreCase(department.getDepartmentCode())) {
+			depDB.setDepartmentCode(department.getDepartmentCode());	
+		}
+		
+		if (Objects.nonNull(department.getDepartmentAddress()) &&
+				!"".equalsIgnoreCase(department.getDepartmentAddress())) {
+			depDB.setDepartmentAddress(department.getDepartmentAddress());
+
+		}
+		
+		return departmentRepository.save(depDB);
 	}
 
 }
