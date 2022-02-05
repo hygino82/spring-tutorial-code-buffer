6095b47b1e57482f808bbb6300f842a854782343
Adroaldo Ferreira <hygino@msn.com>
2022-02-05 18:19:19 -0300

    Adding Loggers in Springboot App

--- src/main/java/com/dailycodebuffer/springboot/tutorial/controller/DepartmentController.java
+++ src/main/java/com/dailycodebuffer/springboot/tutorial/controller/DepartmentController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,14 +23,18 @@ public class DepartmentController {
 
 	@Autowired
 	private DepartmentService departmentService;
+	// insere log nas requisições
+	private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
 
 	@PostMapping("/departments")
 	public Department saveDepartment(@Valid @RequestBody Department department) {
+		LOGGER.info("Inside saveDepartment of DepartmentController");
 		return departmentService.saveDepartment(department);
 	}
 
 	@GetMapping("/departments")
 	public List<Department> fetchDepartmentList() {
+		LOGGER.info("Inside fetchDepartmentList of DepartmentController");
 		return departmentService.fetchDepartmentList();
 	}
 
@@ -36,25 +42,21 @@ public class DepartmentController {
 	public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
 		return departmentService.fetchDepartmentById(departmentId);
 	}
-	
+
 	@DeleteMapping("/departments/{id}")
 	public String deleteDepartmentById(@PathVariable("id") Long departmentId) {
 		departmentService.deleteDepartmentById(departmentId);
-		return"Department deleted successfully!";
+		return "Department deleted successfully!";
 	}
-	
+
 	@PutMapping("/departments/{id}")
-	public Department updateDepartment(
-			@PathVariable("id") Long departmentId,
-			@RequestBody Department department) {
+	public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department) {
 		return departmentService.updateDepartment(departmentId, department);
 	}
 
 	@GetMapping("/departments/name/{name}")
 	public Department fetchDepartmentByName(@PathVariable("name") String departmentNane) {
-		return departmentService.fetchDepartmentByName(departmentNane);	
+		return departmentService.fetchDepartmentByName(departmentNane);
 	}
-	
-	
 
 }
